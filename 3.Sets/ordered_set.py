440d020c4a05dce00aa6d882ba3bdf5c7dc09b50
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2023-08-21 14:54:39 -0600

    ordered sets

---
+++ 3.Sets/ordered_set.py
@@ -0,0 +1,31 @@
+
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+
+class OrdeeredSet(Generic[T]):
+    
+    __data: list[T]
+    
+    def __init__(self, values: list[T] | None = None) -> None:
+        if values is None:
+            self.__data = []
+        else:
+            self.__data = values[::]
+        
+    def add(self, value: T) -> None:
+        if value not in self.__data:
+            self.__data.append(value)
+            
+    def __repr__(self) -> str:
+        return f'OrderedSet({self.__data})'
+    
+    def __len__(self) -> int:
+        return len(self.__data)
+    
+if __name__ == '__main__':
+    a: OrdeeredSet[int] = OrdeeredSet()
+    a.add(5)
+    a.add(7)
+    a.add(5)
+    print(a)
