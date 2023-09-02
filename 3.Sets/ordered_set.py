665983e0ede19a2996823f877895507cb37e9a63
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2023-09-01 20:09:48 -0600

    attention: ordered sets

--- 3.Sets/ordered_set.py
+++ 3.Sets/ordered_set.py
@@ -2,16 +2,32 @@
 from typing import Generic, TypeVar
 
 T = TypeVar('T')
+I = TypeVar('I')
 
-class OrdeeredSet(Generic[T]):
+class OrderedSet(Generic[T]):
+    
+    class __Iterator(Generic[I]):
+        
+        __data: list[I]
+        __current: int 
+        
+        def __init__(self, values: list[I]) -> None:
+            self.__data = values
+            self.__current = 0
+        
+        def __iter__(self) -> Iterator:
+            return self
+        
+        def __next__(self) -> I:
+            result = self.__data[self.__current]
+            self.__current += 1
     
     __data: list[T]
     
-    def __init__(self, values: list[T] | None = None) -> None:
-        if values is None:
-            self.__data = []
-        else:
-            self.__data = values[::]
+    def __init__(self, values: list[T] = []) -> None:
+       self.__data = []
+       for elem in values:
+           self.add(elem)
         
     def add(self, value: T) -> None:
         if value not in self.__data:
@@ -23,9 +39,20 @@ class OrdeeredSet(Generic[T]):
     def __len__(self) -> int:
         return len(self.__data)
     
+    def __contains__(self, value: T) -> bool:
+        return value in self.__data
+    
 if __name__ == '__main__':
-    a: OrdeeredSet[int] = OrdeeredSet()
+    a: OrderedSet[int] = OrderedSet()
     a.add(5)
     a.add(7)
     a.add(5)
-    print(a)
+    print(f'{a = }')
+    print(f'{len(a) = }')
+    b = OrderedSet(["a", "b", "c", "a"])
+    print(b)
+    print(f'{len(b) = }')
+    print(f'{5 in a = }')
+    print(f'{10.2 in a = }')
+    
+    
