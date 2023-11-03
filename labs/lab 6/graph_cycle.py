f3d863ddf33865d9252ecffc76b2cf3c224ec613
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2023-11-03 08:42:07 -0600

    Otro método, otro fallo

--- labs/lab 6/graph_cycle.py
+++ labs/lab 6/graph_cycle.py
@@ -8,6 +8,9 @@
 #           A01753486 Diego Manjarrez Viveros
 # ----------------------------------------------------------
 
+from collections import deque
+import re
+from turtle import position
 from typing import Optional
 
 Graph = dict[str, list[str]]
@@ -18,72 +21,80 @@ https://www.geeksforgeeks.org/detect-cycle-undirected-graph/
 """
 
 
-def has_cycle(initial: str, graph: Graph) -> Optional[list[str]]:
-
-    visited: set[str] = set()
-    stack: list[str] = []
-
-    def dfs(node, parent):
-        visited.add(node)
-        stack.append(node)
-
-        for neighbor in graph[node]:
-            if neighbor == parent:
-                continue
-            if neighbor not in visited:
-                result = dfs(neighbor, node)
-                if result:
-                    return result
-            if neighbor in stack:
-                first = [stack[stack.index(neighbor)]]
-                return stack[stack.index(neighbor):] + first
-        stack.pop()
-
-        return None
-
-    if initial not in visited:
-        cycle = dfs(initial, None)
-        if cycle:
-            return cycle
-    return None
-
 # def has_cycle(initial: str, graph: Graph) -> Optional[list[str]]:
-#     stack: deque[str] = deque()
+
 #     visited: set[str] = set()
-#     visited2: set[str] = set()
-#     stack.append(initial)
-#     while stack:
-#         current: str = stack.pop()
-#         if current not in visited:
-#             yield current
-#             stack.extend(graph[current][::-1])
-#             visited.add(current)
-#         elif (current in visited) and (current not in visited2):
-#             yield current
-#             elems = list(visited)
-#             elems.append(current)
-#             return elems
+#     stack: list[str] = []
+
+#     def dfs(node, parent):
+#         visited.add(node)
+#         stack.append(node)
+
+#         for neighbor in graph[node]:
+#             if neighbor == parent:
+#                 continue
+#             if neighbor not in visited:
+#                 result = dfs(neighbor, node)
+#                 if result:
+#                     return result
+#             if neighbor in stack:
+#                 first = [stack[stack.index(neighbor)]]
+#                 return stack[stack.index(neighbor):] + first
+#         stack.pop()
+
+#         return None
+
+#     if initial not in visited:
+#         cycle = dfs(initial, None)
+#         if cycle:
+#             return cycle
 #     return None
+
+def has_cycle(initial: str, graph: Graph) -> Optional[list[str]]:
+    stack: deque[str] = deque()
+    visited: set[str] = set()
+    visited2: list[str] = []
+    stack.append(initial)
+    result: list[str] = []
+
+    while stack:
+        current: str = stack.pop()
+        if current not in visited:
+            result.append(current)
+            stack.extend(graph[current][::-1])
+            visited.add(current)
+        elif (current in visited) and (current not in visited2):
+            visited2.append(current)
+            position = result.index(current)
+            parent_position = result.index(result[position - 2])
+            if position == 0:
+                break
+            if (parent_position != position):
+                for elem in visited:
+                    if elem == current:
+                        break
+                    visited2.append(elem)
+                return visited2
+    return None
 
 
 if __name__ == '__main__':
     print(has_cycle('A', {
-        'A': ['B', 'C'],
-        'B': ['A', 'C'],
-        'C': ['A', 'B']
+        'A': ['B'],
+        'B': ['A']
     }))
 
     print(has_cycle(
-        'C', {
+        'A', {
             'A': ['B'],
             'B': ['A', 'C'],
-            'C': ['B', 'D'],
-            'D': ['C'],
+            'C': ['B']
         })
     )
 
-    print(has_cycle('A', {
-        'A': ['B', 'C'],
-        'B': ['A', 'C'],
-        'C': ['A', 'B']
-    }))
+    print(has_cycle(
+        'A', {
+            'A': ['B', 'C'],
+            'B': ['A', 'C'],
+            'C': ['A', 'B']
+        }))
